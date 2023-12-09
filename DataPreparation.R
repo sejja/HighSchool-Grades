1c73e2bd52ab05ffaab6939dccbd109baae38718
jaimeetxebarria <jaimeetxebarria@gmail.com>
2023-12-09 18:02:42 +0100

    Fix functions not modifying dataframe + refactor prints

--- DataPreparation.R
+++ DataPreparation.R
@@ -1,4 +1,5 @@
 convert_types <- function(dataset) {
+  print("Convert into appropiate types")
   logical_fields <- c("school_support", "family_support", "extra_paid_classes", "activities", "nursery_school", "higher_ed", "internet_access", "romantic_relationship")
   factor_fields <- c("school", "sex", "address_type", "family_size", "parent_status", "mother_education", "father_education", "mother_job", "father_job", "school_choice_reason", "guardian", "travel_time", "study_time")
   
@@ -13,6 +14,7 @@ convert_types <- function(dataset) {
 }
 
 swap_rows <- function(dataset) {
+  print("Swap rows for tydiness")
   names <- names(dataset)
   names[16] <- "romantic_relationship"
   names[24] <- "class_failures"
@@ -21,6 +23,7 @@ swap_rows <- function(dataset) {
 }
 
 find_outliers <- function(dataset){
+  print("Searching outliers")
   important_numeric_fields <- c("absences","age","weekday_alcohol","weekend_alcohol","health")
   outliers_list <- lapply(dataset[important_numeric_fields], boxplot_outliers)
   # Hay que ver cuales quitamos y cuales dejamos --------------------------------------------------------------------------------
@@ -34,6 +37,7 @@ boxplot_outliers <- function(column) {
 
 #Adds 2% NA values to the data set in random positions in column health
 generate_na_values <- function(dataset) {
+  print("Adding artificial NA values")
   set.seed(0)
   na_index <- sample(nrow(dataset), 0.02 * nrow(dataset))
   dataset[na_index, "health"] <- NA
@@ -48,32 +52,31 @@ na_imputation <- function(dataset){
 }
 
 normal_column <- function(dataset){
-  dataset <- cbind(dataset, normalized_health = ((dataset$health - min(dataset$health))/(max(dataset$health)-min(dataset$health))))
+  print("Adding normalized health column")
+  dataset$normalized_health <- (dataset$health - min(dataset$health))/(max(dataset$health)-min(dataset$health))
+  return(dataset)
 }
 
 combined_column <- function(dataset){
-  dataset <- cbind(dataset, week_alcohol = (dataset$weekday_alcohol + dataset$weekend_alcohol)/2)
+  print("Adding combined alcohol column")
+  dataset$week_alcohol <- (dataset$weekday_alcohol + dataset$weekend_alcohol)/2
+  return(dataset)
 }
 
 remove_excess_rows <- function(dataset) {
+  print("Removing uneeded rows")
   dataset$student_id <- NULL
+  return(dataset)
 }
 
 prepare_data <- function(df) {
-  print("Data Set Read Succesfully!")
-  print("Removing uneeded rows")
-  remove_excess_rows(df)
-  print("Convert into appropiate types")
+  df <- remove_excess_rows(df)
   df <- convert_types(df)
-  print("Swap rows for tydiness")
   df <- swap_rows(df)
-  print("Searching outliers")
   find_outliers(df)
-  print("Adding artificial NA values")
   df <- generate_na_values(df)
   df <- na_imputation(df)
-  print("Adding normalized health column")
-  normal_column(df)
-  combined_column(df)
+  df <- normal_column(df)
+  df <- combined_column(df)
   return (df)
 }
